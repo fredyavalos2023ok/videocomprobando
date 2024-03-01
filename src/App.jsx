8043ed9fcc405f954ed2c707dc3a11b12f702e90
afredy4 <afredy.ronaldo4@gmail.com>
2024-03-01 07:00:34 -0500

    Este es el primer cambio luego de eliminar test

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React Este es el primer cambio luego de eliminar test</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
